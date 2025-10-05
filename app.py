46183b8f1c3c7bb1f2ab5e1be2b7da7056103da4
kometa-star <sinfo7998@gmail.com>
2025-10-05 12:52:57 +0500

    Update app.py

--- app.py
+++ app.py
@@ -259,4 +259,4 @@ app.config['TEMPLATES_AUTO_RELOAD'] = True
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0  
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=PORT))
+    app.run(debug=True, host='0.0.0.0', port=PORT)
